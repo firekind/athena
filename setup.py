37faf5843bbe719a741d644cf005f108ad8c70a3
firekind <shyamant.achar@gmail.com>
2020-09-12 12:49:59 +0530

    updated python version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.0.1",
     author="Shyamant Achar",
     packages=setuptools.find_packages(),
-    python_requires='>=3.7',
+    python_requires='>=3.6.8',
     install_requires=[
         "pkbar==0.4",
         "torchsummary==1.5.1",
