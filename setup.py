471a98962d86eed5a47ea68861f81d2e95fc196a
firekind <shyamant.achar@gmail.com>
2020-09-12 12:56:35 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setuptools.setup(
     python_requires='>=3.6.8',
     install_requires=[
         "pkbar==0.4",
-        "torchsummary==1.5.1",
-        "tqdm==4.48.2",
-        "matplotlib==3.3.1"
+        "torchsummary",
+        "tqdm",
+        "matplotlib"
     ]
 )
