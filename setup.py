56d5626d929d5322f4fbb0ae464c5f5131e889ab
firekind <shyamant.achar@gmail.com>
2020-09-23 20:05:22 +0530

    updated setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires='>=3.6.8',
     install_requires=[
-        "torchsummary~=0.7.0",
         "tqdm~=4.48.2",
         "matplotlib~=3.3.1",
         "tensorboard~=2.3.0",
